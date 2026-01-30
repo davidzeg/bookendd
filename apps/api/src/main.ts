a37bb4c863a7f2f50705991ee1fa4dbe45622203
David Gjorgoski <david_gjorgoski@hotmail.com>
2026-01-30 03:34:14 +0100

    feat: add tRPC router skeleton

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { trpcMiddleware } from './trpc/trpc.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use('/trpc', trpcMiddleware);
+
   await app.listen(process.env.PORT ?? 3001);
+
+  console.log('API running on http://localhost:3001');
+  console.log('tRPC available at http://localhost:3001/trpc');
 }
 bootstrap();
